72d8d471d358e447d4f9bf66d7ee1cfe86e1dd77
Marcelo Rosa <marcelo.rosa.br@gmail.com>
2018-12-09 20:11:04 -0200

    Inclusao Metodo

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsumeWebApi_1
 {
-    class Program
+   class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             ProcessRepositories("01606302018", "58092582018").Wait();
+        }
+    
+        private static async Task ProcessRepositories(string strDt, string strGmci)
+        {
+           
+            string URI;
+            URI = "http://gmcieletronica.btp.com.br:8084/Avaria?Dt=" + strDt + "&Gmci=" + strGmci + "";
+            
+            var stringTask = client.GetStringAsync(URI);
+
+            var msg = await stringTask;
+            Console.Write(msg);
         }
     }
 }

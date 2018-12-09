ecd588efc046dbb7bdc66917affbf862bddf5858
Marcelo Rosa <marcelo.rosa.br@gmail.com>
2018-12-09 20:31:17 -0200

    Inicio Jason

---
+++ Model/Avaria.cs
@@ -0,0 +1,9 @@
+public class Avaria
+{
+    public string CodTipo { get; set; }
+    public string CodLocal { get; set; }
+    public string CodComplemento { get; set; }
+    public string DescTipo { get; set; }
+    public string DescLocal { get; set; }
+    public string DescComplemento { get; set; }
+}

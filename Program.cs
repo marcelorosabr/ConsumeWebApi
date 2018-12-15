cf2e61ea13d1e374266ce698c765725443532239
Marcelo Rosa <marcelo.rosa.br@gmail.com>
2018-12-15 17:02:05 -0200

    task

--- Program.cs
+++ Program.cs
@@ -12,9 +12,27 @@ namespace ConsumeWebApi_1
         
         static void Main(string[] args)
         {
-             ProcessRepositories("01606302018", "58092582018").Wait();
+             //ProcessRepositories("01606302018", "58092582018").Wait();
+             ContestarAvaria("1","1","1","1").Wait();
         }
     
+         private static async Task ContestarAvaria(string strDt, string strGmci, string strLocal, string strTipo)
+        {
+                string URI = "";
+
+                URI = "http://gmcieletronica.btp.com.br:8084/Contestar?Dt=" + strDt + "&Gmci=" + strGmci + "&CodLocal=" + strLocal + "&CodTipo=" + strTipo + "";
+               
+               // client.PostAsync(URI, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+
+                var streamTask = client.GetStreamAsync(URI);
+                var serializer = new DataContractJsonSerializer(typeof(String));
+                var repositories = serializer.ReadObject(await streamTask) as string;
+                Console.WriteLine( repositories);
+                
+        }
+
+
+        
         private static async Task ProcessRepositories(string strDt, string strGmci)
         {
            
@@ -39,7 +57,5 @@ namespace ConsumeWebApi_1
             }   
               
         }
-
-
     }
 }
